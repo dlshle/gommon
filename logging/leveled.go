02291dc657b47b03c26cff0d979576fd875009d8
Xuri Li <xuri.li@cloudkitchens.com>
2023-02-18 22:09:14 +0800

    go-flavored logging interface and supports

---
+++ logging/leveled.go
@@ -0,0 +1,230 @@
+package logging
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"strconv"
+	"time"
+
+	"github.com/dlshle/gommon/stringz"
+)
+
+type LevelLogger struct {
+	writer            LogWriter
+	prefix            string
+	logLevelWaterMark int
+	context           map[string]string
+	enableGRContext   bool
+	subLoggers        []Logger
+}
+
+const LogAllWaterMark = -1
+
+var nilStringBytes = []byte{'n', 'i', 'l'}
+
+func StdOutLevelLogger(prefix string) Logger {
+	return CreateLevelLogger(NewConsoleLogWriter(os.Stdout), prefix, LogAllWaterMark)
+}
+
+func NewLevelLogger(writer io.Writer, prefix string, format int, waterMark int) Logger {
+	return &LevelLogger{
+		writer:            NewConsoleLogWriter(writer),
+		prefix:            prefix,
+		logLevelWaterMark: waterMark,
+		context:           make(map[string]string),
+		enableGRContext:   false,
+		subLoggers:        make([]Logger, 0),
+	}
+}
+
+func CreateLevelLogger(entityWriter LogWriter, prefix string, loggingMark int) Logger {
+	return &LevelLogger{
+		writer:            entityWriter,
+		prefix:            prefix,
+		logLevelWaterMark: loggingMark,
+		context:           make(map[string]string),
+		enableGRContext:   true,
+		subLoggers:        make([]Logger, 0),
+	}
+}
+
+func (l *LevelLogger) output(ctx context.Context, level int, data ...string) {
+	if level < l.logLevelWaterMark {
+		return
+	}
+	var builder bytes.Buffer
+	if data == nil {
+		builder.Write(nilStringBytes)
+	} else if len(data) == 1 {
+		builder.WriteString(data[0])
+	} else {
+		builder.WriteString(stringz.ConcatString(data...))
+	}
+	logEntity := newLogEntity(level, l.prefix, l.prepareContext(ctx), time.Now(), builder.String(), l.getFileName())
+	l.writer.Write(logEntity)
+	logEntity.recycle()
+}
+
+func (l *LevelLogger) getFileName() string {
+	_, file, line, ok := runtime.Caller(3)
+	if !ok {
+		file = "???"
+		line = 0
+	}
+	short := file
+	for i := len(file) - 1; i > 0; i-- {
+		if file[i] == '/' {
+			short = file[i+1:]
+			break
+		}
+	}
+	file = short
+	return file + ":" + strconv.Itoa(line)
+}
+
+func (l *LevelLogger) prepareContext(ctx context.Context) map[string]string {
+	allContext := make(map[string]string)
+	for k, v := range l.context {
+		allContext[k] = v
+	}
+	iLoggingCtx := ctx.Value(CtxValLoggingContext)
+	if iLoggingCtx != nil {
+		loggingCtx, ok := iLoggingCtx.(map[string]string)
+		if ok {
+			for k, v := range loggingCtx {
+				allContext[k] = v
+			}
+		}
+	}
+	return allContext
+}
+
+func (l *LevelLogger) Debug(ctx context.Context, records ...string) {
+	l.output(ctx, DEBUG, records...)
+}
+
+func (l *LevelLogger) Trace(ctx context.Context, records ...string) {
+	l.output(ctx, TRACE, records...)
+}
+
+func (l *LevelLogger) Info(ctx context.Context, records ...string) {
+	l.output(ctx, INFO, records...)
+}
+
+func (l *LevelLogger) Warn(ctx context.Context, records ...string) {
+	l.output(ctx, WARN, records...)
+}
+
+func (l *LevelLogger) Error(ctx context.Context, records ...string) {
+	l.output(ctx, ERROR, records...)
+}
+
+func (l *LevelLogger) Fatal(ctx context.Context, records ...string) {
+	l.output(ctx, FATAL, records...)
+}
+
+func (l *LevelLogger) Debugf(ctx context.Context, format string, records ...interface{}) {
+	l.output(ctx, DEBUG, fmt.Sprintf(format, records...))
+}
+
+func (l *LevelLogger) Tracef(ctx context.Context, format string, records ...interface{}) {
+	l.output(ctx, TRACE, fmt.Sprintf(format, records...))
+}
+
+func (l *LevelLogger) Infof(ctx context.Context, format string, records ...interface{}) {
+	l.output(ctx, INFO, fmt.Sprintf(format, records...))
+}
+
+func (l *LevelLogger) Warnf(ctx context.Context, format string, records ...interface{}) {
+	l.output(ctx, WARN, fmt.Sprintf(format, records...))
+}
+
+func (l *LevelLogger) Errorf(ctx context.Context, format string, records ...interface{}) {
+	l.output(ctx, ERROR, fmt.Sprintf(format, records...))
+}
+
+func (l *LevelLogger) Fatalf(ctx context.Context, format string, records ...interface{}) {
+	l.output(ctx, FATAL, fmt.Sprintf(format, records...))
+}
+
+func (l *LevelLogger) SetContext(k, v string) {
+	l.context[k] = v
+}
+
+func (l *LevelLogger) DeleteContext(k string) {
+	delete(l.context, k)
+}
+
+func (l *LevelLogger) Prefix(prefix string) {
+	l.prefix = prefix
+}
+
+func (l *LevelLogger) PrefixWithPropogate(prefix string) {
+	l.prefix = prefix
+	for _, subLogger := range l.subLoggers {
+		subLogger.PrefixWithPropogate(prefix)
+	}
+}
+
+func (l *LevelLogger) Format(format int) {
+	// no-op
+}
+
+func (l *LevelLogger) Writer(writer LogWriter) {
+	l.writer = writer
+}
+
+func (l *LevelLogger) WriterWithPropogate(writer LogWriter) {
+	l.writer = writer
+	for _, subLogger := range l.subLoggers {
+		subLogger.WriterWithPropogate(writer)
+	}
+}
+
+// create new logger
+func (l *LevelLogger) WithPrefix(prefix string) Logger {
+	subLogger := CreateLevelLogger(l.writer, prefix, l.logLevelWaterMark)
+	l.subLoggers = append(l.subLoggers, subLogger)
+	return subLogger
+}
+
+func (l *LevelLogger) WithFormat(format int) Logger {
+	subLogger := CreateLevelLogger(l.writer, l.prefix, l.logLevelWaterMark)
+	l.subLoggers = append(l.subLoggers, subLogger)
+	return subLogger
+}
+
+func (l *LevelLogger) WithWriter(writer LogWriter) Logger {
+	subLogger := CreateLevelLogger(writer, l.prefix, l.logLevelWaterMark)
+	l.subLoggers = append(l.subLoggers, subLogger)
+	return subLogger
+}
+
+func (l *LevelLogger) WithGRContextLogging(useGRCL bool) Logger {
+	subLogger := &LevelLogger{
+		writer:            l.writer,
+		prefix:            l.prefix,
+		logLevelWaterMark: l.logLevelWaterMark,
+		context:           l.context,
+		enableGRContext:   useGRCL,
+		subLoggers:        make([]Logger, 0),
+	}
+	l.subLoggers = append(l.subLoggers, subLogger)
+	return subLogger
+}
+
+func (l *LevelLogger) WithContext(context map[string]string) Logger {
+	subLogger := &LevelLogger{
+		writer:            l.writer,
+		prefix:            l.prefix,
+		logLevelWaterMark: l.logLevelWaterMark,
+		context:           context,
+		subLoggers:        make([]Logger, 0),
+	}
+	l.subLoggers = append(l.subLoggers, subLogger)
+	return subLogger
+}
---
+++ logging/logger.go
@@ -0,0 +1,207 @@
+package logging
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/dlshle/gommon/utils"
+)
+
+var logEntityPool sync.Pool
+
+func init() {
+	logEntityPool = sync.Pool{
+		New: func() any {
+			return new(LogEntity)
+		},
+	}
+}
+
+var GlobalLogger Logger = CreateLevelLogger(NewConsoleLogWriter(os.Stdout), "", LogAllWaterMark)
+
+func SetLogger(logger Logger) {
+	GlobalLogger = logger
+}
+
+const CtxValLoggingContext = "$logging_ctx"
+
+const (
+	TRACE = iota
+	DEBUG
+	INFO
+	WARN
+	ERROR
+	FATAL
+
+	pTrace = "[TRACE]"
+	pDebug = "[DEBUG]"
+	pInfo  = "[INFO]"
+	pWarn  = "[WARN]"
+	pError = "[ERROR]"
+	pFatal = "[FATAL]"
+)
+
+var LogLevelPrefixMap = map[int]string{
+	TRACE: pTrace,
+	DEBUG: pDebug,
+	INFO:  pInfo,
+	WARN:  pWarn,
+	ERROR: pError,
+	FATAL: pFatal,
+}
+
+type Logger interface {
+	Trace(ctx context.Context, records ...string)
+	Debug(ctx context.Context, records ...string)
+	Info(ctx context.Context, records ...string)
+	Warn(ctx context.Context, records ...string)
+	Error(ctx context.Context, records ...string)
+	Fatal(ctx context.Context, records ...string)
+
+	Tracef(ctx context.Context, format string, records ...interface{})
+	Debugf(ctx context.Context, format string, records ...interface{})
+	Infof(ctx context.Context, format string, records ...interface{})
+	Warnf(ctx context.Context, format string, records ...interface{})
+	Errorf(ctx context.Context, format string, records ...interface{})
+	Fatalf(ctx context.Context, format string, records ...interface{})
+
+	SetContext(k, v string)
+	DeleteContext(k string)
+	Prefix(prefix string)
+	PrefixWithPropogate(prefix string)
+	Format(format int)
+	Writer(writer LogWriter)
+	WriterWithPropogate(writer LogWriter)
+
+	// create new logger
+	WithPrefix(prefix string) Logger
+	WithFormat(format int) Logger
+	WithWriter(writer LogWriter) Logger
+
+	WithContext(context map[string]string) Logger
+	WithGRContextLogging(bool) Logger
+}
+
+type LogEntity struct {
+	Level     int
+	Prefix    string
+	Context   map[string]string
+	Timestamp time.Time
+	Message   string
+	File      string
+}
+
+func (e *LogEntity) recycle() {
+	logEntityPool.Put(e)
+}
+
+func newLogEntity(level int, prefix string, context map[string]string, timestamp time.Time, message string, file string) *LogEntity {
+	entity := logEntityPool.Get().(*LogEntity)
+	entity.Level = level
+	entity.Prefix = prefix
+	entity.Context = context
+	entity.Timestamp = timestamp
+	entity.Message = message
+	entity.File = file
+	return entity
+}
+
+type LogWriter interface {
+	Write(entity *LogEntity)
+}
+
+type SimpleStringWriter struct {
+	consoleWriter io.Writer
+}
+
+func NewConsoleLogWriter(writer io.Writer) LogWriter {
+	return SimpleStringWriter{
+		writer,
+	}
+}
+
+func (w SimpleStringWriter) Write(logEntity *LogEntity) {
+	var builder bytes.Buffer
+	builder.WriteString(logEntity.Timestamp.Format(time.RFC3339))
+	builder.WriteRune(' ')
+	builder.WriteString(LogLevelPrefixMap[logEntity.Level])
+	builder.WriteRune(' ')
+	builder.WriteString(logEntity.Prefix)
+	builder.WriteRune(' ')
+	builder.WriteString(logEntity.File)
+	builder.WriteRune(' ')
+	// contexts
+	contexts := logEntity.Context
+	ctxLen := len(contexts)
+	if ctxLen > 0 {
+		builder.WriteRune(' ')
+		ctxCnt := 0
+		builder.WriteRune('{')
+		for k, v := range contexts {
+			builder.WriteString(k)
+			builder.WriteRune(':')
+			builder.WriteString(v)
+			ctxCnt++
+			if ctxCnt < ctxLen {
+				builder.WriteRune(';')
+			}
+		}
+		builder.WriteString("} ")
+	}
+	builder.WriteString(logEntity.Message)
+	builder.WriteRune('\n')
+	w.consoleWriter.Write(builder.Bytes())
+}
+
+type JSONWriter struct {
+	ioWriter io.Writer
+}
+
+func NewJSONWriter(ioWriter io.Writer) LogWriter {
+	return JSONWriter{
+		ioWriter: ioWriter,
+	}
+}
+
+func (w JSONWriter) Write(entity *LogEntity) {
+	w.ioWriter.Write(w.getJSONEntityBytes(entity))
+}
+
+func (w JSONWriter) getJSONEntityBytes(entity *LogEntity) []byte {
+	var builder bytes.Buffer
+	builder.WriteRune('{')
+	w.writeKVPair(builder, w.quoteWith("timestamp"), w.quoteWith(entity.Timestamp.Format(time.RFC3339)))
+	builder.WriteRune(',')
+	w.writeKVPair(builder, w.quoteWith("file"), w.quoteWith(entity.File))
+	builder.WriteRune(',')
+	w.writeKVPair(builder, w.quoteWith("level"), strconv.Itoa(entity.Level))
+	builder.WriteRune(',')
+	w.writeKVPair(builder, w.quoteWith("prefix"), w.quoteWith(entity.Prefix))
+	builder.WriteRune(',')
+	w.writeKVPair(builder, w.quoteWith("message"), w.quoteWith(entity.Message))
+	builder.WriteRune(',')
+	w.writeKVPair(builder, w.quoteWith("context"), utils.StringMapToJSON(entity.Context))
+	builder.WriteRune('}')
+	return builder.Bytes()
+}
+
+func (w JSONWriter) quoteWith(val string) string {
+	return "\"" + val + "\""
+}
+
+func (w JSONWriter) writeKVPair(b bytes.Buffer, k, v string) string {
+	return k + ":" + v
+}
+
+type NoopWriter struct{}
+
+func NewNoopWriter() NoopWriter {
+	return NoopWriter{}
+}
+
+func (w NoopWriter) Write(entity *LogEntity) {}

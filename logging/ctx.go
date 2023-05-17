7e1b1bbaf3df2ea2f2dfb4782b9a77f20f6639d2
Xuri Li <xuri.li@cloudkitchens.com>
2023-05-18 00:01:42 +0800

    use copy for logging context

--- logging/ctx.go
+++ logging/ctx.go
@@ -6,7 +6,11 @@ func WrapCtx(ctx context.Context, key, val string) context.Context {
 	originalValue := ctx.Value(CtxValLoggingContext)
 	var mapCtx map[string]string
 	if originalValue != nil {
-		mapCtx = originalValue.(map[string]string)
+		original := originalValue.(map[string]string)
+		mapCtx = make(map[string]string)
+		for k, v := range original {
+			mapCtx[k] = v
+		}
 	}
 	if mapCtx == nil {
 		mapCtx = make(map[string]string)

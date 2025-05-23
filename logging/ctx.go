6129e0b4c791140ce8dd7379209f43511b11ba9d
Xuri Li <xuri.li@cloudkitchens.com>
2025-05-23 10:35:14 +0800

    use deep copy for ctx map

--- logging/ctx.go
+++ logging/ctx.go
@@ -3,12 +3,16 @@ package logging
 import "context"
 
 func WrapCtx(ctx context.Context, key, val string) context.Context {
-	var (
-		mapCtx map[string]string = nil
-		ok     bool
-	)
 	originalValue := ctx.Value(CtxValLoggingContext)
-	if mapCtx, ok = originalValue.(map[string]string); mapCtx == nil || !ok {
+	var mapCtx map[string]string
+	if originalValue != nil {
+		original := originalValue.(map[string]string)
+		mapCtx = make(map[string]string)
+		for k, v := range original {
+			mapCtx[k] = v
+		}
+	}
+	if mapCtx == nil {
 		mapCtx = make(map[string]string)
 	}
 	mapCtx[key] = val
